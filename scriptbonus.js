b640a391448319ce072d59ccc4d2475d2b7de008
MartinaBasile <martinaleandribasile@gmail.com>
2022-09-05 10:05:01 +0200

    def bonus file used sort method... in comments solution with a cicle while

--- scriptbonus.js
+++ scriptbonus.js
@@ -1,4 +1,4 @@
-console.log("Js ok")
+console.log("Js ok bonus")
 
 const btnStart = document.querySelector(".btn-success");
 const gridBox = document.querySelector(".grid");
@@ -7,20 +7,20 @@ let cells = [];
 
 
 //funzioni
-function randomNumberFunction(min, max) {
+/*function randomNumberFunction(min, max) {
     numberGenerate = Math.floor(Math.random() * max) + min;
     return numberGenerate
-}
+}*/
 
 function changeColor(cell) {
     let classColor = parseInt(cell.innerHTML) % 2 === 0 ? "bg-success" : "bg-danger"
     cell.classList.add(classColor)
 }
 
-function generateArrayCells(minindex, maxindex) {
+function generateArrayCells(maxindex) {
+    let arrayNumbers = generateNumberArray()
     for (let i = 0; i < maxindex; i++) {
         let cell = document.createElement("div");
-        let arrayNumbers = generateNumberArray(100)
         cell.className = "square"
         cell.innerHTML = arrayNumbers[i];
         cell.addEventListener("click", function () {
@@ -32,32 +32,38 @@ function generateArrayCells(minindex, maxindex) {
     return gridBox
 }
 
-function generateNumberArray(max) {
-    let array = []
-    while (array.length < max) {
-        let number = randomNumberFunction(1, 100);
-        console.log(number)
-        let numbernew = true;
-        let i = 0
-        do {
-            if (number === array[i]) {
-                numbernew = false;
-            }
-            i++
-        } while (i < array.length)
-
-        if (numbernew) {
-            array.push(number)
-        }
+function generateNumberArray() {
+    let arrayNumbers = []
+    for (let index = 1; index <= 100; index++) {
+        arrayNumbers.push(index)
     }
-    return array
+    console.log(arrayNumbers)
+    arrayNumbers.sort(function () {
+        let random = Math.round(Math.random())
+        return random === 1 ? 1 : -1;
+    })
+    return arrayNumbers
+
 }
+/*
+
+function alternativeRandomArray(){
+let array=[]
+while (array.length < 100) {
+    let number = randomNumberFunction(1, 100);
+   if (!array.includes(number)){
+    array.push(number)
+   }
+}
+return array
+}
+*/
 
 
 // codice al click start
 btnStart.addEventListener("click", function () {
     container.innerHTML = ""
-    cells = generateArrayCells(1, 100);
+    cells = generateArrayCells(100);
     container.appendChild(gridBox);
 
 })
